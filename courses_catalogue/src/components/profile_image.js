4cb0998137daf6c42c5a80955dbbfda4af6e0e0a
Joanna Kuo <jkuo@princeton.edu>
2021-01-11 21:23:17 -0500

    added box shadow to img

--- courses_catalogue/src/components/profile_image.js
+++ courses_catalogue/src/components/profile_image.js
@@ -12,6 +12,8 @@ const useStyles = makeStyles(() => ({
     objectFit: "cover",
     left: "50%",
     transform: "translate(-50%, -50%);",
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
   },
   name: {
     position: "fixed",
